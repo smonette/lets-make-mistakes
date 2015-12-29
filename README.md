9533edd726875fa0da26d3bf8deda23dfdc13b63
Steph Monette <steph@muledesign.com>
2015-12-28 16:01:32 -0800

    commiting mirror of the LMM redesign

---
+++ README.md
@@ -0,0 +1,25 @@
+# Let's Make Mistakes
+
+Repo for the 2015 [Let's Make Mistakes](http://mistakes.show) site.
+
+### Table of Contents
+* [Tech Overview](#tech-overview)
+* [Included Packages](#included-packages)
+
+---
+
+
+## Tech Overview
+This is [Node.js](https://nodejs.org/) app, that uses Node Soundcloud API to update the episode list. The Sass is compiled using [Grunt](http://gruntjs.com/).
+
+The Sass is compiled by Grunt. From the home directory run `grunt watch` and Grunt will automatically compile changes as you work on the site.
+
+## Included Packages
+* ejs
+* express
+* node-sass
+* node-soundcloud
+* nodemon 
+* grunt
+* grunt-sass
+* grunt-contrib-watch 
---
+++ app.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var app = require('express')();
+var http = require('http').createServer(app);
+
+// Include Soundcloud
+var SC = require('node-soundcloud');
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+ 
+// Initialize client
+SC.init({
+  id: '6e7d6bb6fab87f38f69a857e4544b21d',
+  secret: '18a1dca02b5e86e2058fb93c7a4949eb',
+  uri: 'http://www.muleradio.net/callback'
+});
+
+
+SC.get('/users/57850026/tracks', function(err, track) {
+  if ( err ) {
+    console.log('well shit, soundcloud didnt load');
+  } else {
+    console.log('omfg it worked!:', track);
+    // var allTracks = JSON.parse(track);
+    allTracks = track;
+  }
+});
+
+// VIEWS
+app.get('/', function(req,res){
+  res.render('pages/index', {shows: allTracks});
+});
+
+app.get('/episode/:id', function(req,res){
+  id = req.params.id;
+
+  SC.get('/tracks/'+ id, function(err, track) {
+    if ( err ) {
+      console.log('well shit, soundcloud didnt load');
+    } else {
+      console.log('omfg it worked!:', track);
+      // var allTracks = JSON.parse(track);
+      singleTrack = track;
+    }
+  });
+
+  res.render('pages/episode', {show: singleTrack});
+});
+
+// START THE SERVER
+
+http.listen(process.env.PORT || 3000, function(){
+  console.log("local hosties");
+});
+
+
